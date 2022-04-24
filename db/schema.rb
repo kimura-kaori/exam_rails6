ad0dbe6ae298757d58793bc0af5c638ba6df0190
kaori kimura <tkhy_1125@icloud.com>
2022-04-24 13:25:28 +0900

    アソシエーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_084607) do
+ActiveRecord::Schema.define(version: 2022_04_24_034527) do
 
   create_table "nearest_stations", force: :cascade do |t|
     t.string "route_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_04_22_084607) do
     t.integer "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "property_id", null: false
+    t.index ["property_id"], name: "index_nearest_stations_on_property_id"
   end
 
   create_table "properties", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_04_22_084607) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "nearest_stations", "properties"
 end
